1af984d7b2d5d94aa7e8e9a2aac64102c18e651a
Daniel.fasuba <66290818+Danielfasuba@users.noreply.github.com>
2020-06-06 14:56:13 +0100

    HNG-05315
    I updated my script, forgot to add my "email variable"

--- scripts/HNG-05315.py
+++ scripts/HNG-05315.py
@@ -1,5 +1,7 @@
 full_name = 'Fasuba Daniel'
 hng_id = '05315'
+language = 'python'
+email = 'danielfasuba@gmail.com'
 string = f"Hello world, this is {full_name} with HNGi7 ID HNG-{hng_id} using python for stage 2 task "
 
 print(string)
